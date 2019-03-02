a276dacb388095f65372e5c109618a9edaf41b3d
Nbiba Bedis <bedisnbiba@gmail.com>
2019-03-02 17:55:13 +0100

    more gui work

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Med {
     boxes: HashMap<String, Box>,
     wins: HashMap<String, Window>,
     database: RefMap<String, PathBuf>,
-    patients_btns: RefMap<String, Button>,
+    patients_rows: RefMap<String, Box>,
 }
 
 impl Default for Med {
@@ -34,7 +34,7 @@ impl Default for Med {
             entrys: HashMap::new(),
             boxes: HashMap::new(),
             wins: HashMap::new(),
-            patients_btns: Rc::new(RefCell::new(HashMap::new())),
+            patients_rows: Rc::new(RefCell::new(HashMap::new())),
             database,
         }
     }
@@ -54,7 +54,7 @@ impl Med {
         vbox.add(&hbox);
         vbox.add(&search_bar);
         for name in self.database.borrow().keys() {
-            Self::sig_patient_add(&self.patients_btns, name.to_string(), &vbox, None, None)
+            Self::sig_patient_add(&self.patients_rows, name.to_string(), &vbox, None, None)
         }
 
         let win = Window::new(WindowType::Toplevel);
@@ -80,8 +80,6 @@ impl Med {
 
     // parse
     fn parse_database() -> HashMap<String, PathBuf> {
-        use std::fs;
-
         let mut map_base = HashMap::new();
         let med_dir = Self::med_dir();
 
@@ -102,7 +100,7 @@ impl Med {
         // clone ref
         let search_bar = self.entrys["search"].clone();
         let vbox = self.boxes["vbox"].clone();
-        let p_b = self.patients_btns.clone();
+        let p_b = self.patients_rows.clone();
 
         search_bar.connect_property_text_length_notify(move |search_bar| {
             let search = search_bar.get_text().unwrap().to_string();
@@ -126,7 +124,7 @@ impl Med {
     fn sig_add_patients_btn(&mut self) {
         // clone ref
         let vbox = self.boxes["vbox"].clone();
-        let patients_btns = self.patients_btns.clone();
+        let patients_rows = self.patients_rows.clone();
         let database = self.database.clone();
         //
         self.btns["add"].connect_clicked(move |_btn| {
@@ -138,13 +136,13 @@ impl Med {
             // clone ref
             let vbox = vbox.clone();
             let ew = entry_win.clone();
-            let patients_btns = patients_btns.clone();
+            let patients_rows = patients_rows.clone();
             let database = database.clone();
             //
 
             entry_text.connect_activate(move |et| {
                 Self::sig_patient_add(
-                    &patients_btns,
+                    &patients_rows,
                     et.get_text().unwrap().to_string(),
                     &vbox,
                     Some(&database),
@@ -156,14 +154,12 @@ impl Med {
     }
 
     fn sig_patient_add(
-        patients_btns: &RefMap<String, Button>,
+        patients_rows: &RefMap<String, Box>,
         patient_name: String,
         vbox: &Box,
         database: Option<&RefMap<String, PathBuf>>,
         ew: Option<&Window>,
     ) {
-        let btn = Button::new_with_label(&patient_name);
-
         //clone
         let pname = patient_name.clone();
 
@@ -193,38 +189,10 @@ impl Med {
         //     Self::sig_save_patient_data(&p_dir, patient_name.clone(), "".to_string());
         // }
 
-        btn.connect_clicked(move |_btn| {
-            let entry_text = Entry::new();
-            let save_btn = Button::new_with_label("Save");
-            let vbox = Box::new(Orientation::Vertical, 10);
+        let hbox = Self::create_patient_row(patient_name, p_dir);
 
-            vbox.pack_start(&entry_text, true, true, 10);
-            vbox.pack_start(&save_btn, false, false, 10);
-
-            let entry_win = Window::new(WindowType::Toplevel);
-            entry_win.add(&vbox);
-            entry_win.set_title(&patient_name);
-            entry_text.set_text(&Self::read_patient_data(&p_dir).1);
-
-            // clone ref
-            let patient_name = RefCell::new(patient_name.clone());
-            let ew = entry_win.clone();
-            let et = entry_text.clone();
-
-            let p_dir = p_dir.clone();
-
-            save_btn.connect_clicked(move |_| {
-                let p_name = patient_name.borrow().to_string();
-                let p_data = et.get_text().unwrap().to_string();
-                Self::sig_save_patient_data(&p_dir, p_name, p_data);
-                ew.destroy();
-            });
-
-            entry_win.show_all();
-        });
-
-        vbox.add(&btn);
-        patients_btns.borrow_mut().insert(pname, btn);
+        vbox.add(&hbox);
+        patients_rows.borrow_mut().insert(pname, hbox);
 
         vbox.show_all();
 
@@ -255,7 +223,7 @@ impl Med {
     fn sig_remove_patient_btn(&mut self) {
         // ref clone
         let big_vbox = self.boxes["vbox"].clone();
-        let patients_btns = self.patients_btns.clone();
+        let patients_rows = self.patients_rows.clone();
         let database = self.database.clone();
 
         self.btns["rm"].connect_clicked(move |_btn| {
@@ -274,7 +242,7 @@ impl Med {
             entry_win.show_all();
 
             //ref clone
-            let patients_btns = patients_btns.clone();
+            let patients_rows = patients_rows.clone();
             let et = entry_text.clone();
             let ew = entry_win.clone();
             let big_vbox = big_vbox.clone();
@@ -282,7 +250,7 @@ impl Med {
 
             rm_btn.connect_clicked(move |_rm_btn| {
                 let p_name = et.get_text().unwrap().to_string();
-                let p_btn = &patients_btns.borrow()[&p_name];
+                let p_btn = &patients_rows.borrow()[&p_name];
                 let p_dir = &database.borrow()[&p_name];
 
                 let _ = fs::remove_dir_all(&p_dir);
@@ -291,6 +259,118 @@ impl Med {
                 ew.destroy();
             });
         });
+    }
+    // gui pieces
+    fn create_patient_row(patient_name: String, p_dir: PathBuf) -> Box {
+        // name diag de ds nd
+        let (diag, de, ds, nd) = Self::read_patient_header(&p_dir);
+
+        // name button
+        let btn = Button::new_with_label(&patient_name);
+
+        btn.connect_clicked(move |_btn| {
+            Self::create_patient_win(&patient_name, &p_dir);
+        });
+
+        // the rest
+        let hbox = Box::new(Orientation::Horizontal, 10);
+        hbox.add(&btn);
+        for col in [diag, de, ds, nd].iter() {
+            let label = Label::new(col.as_str());
+            hbox.add(&label);
+        }
+
+        hbox
+    }
+
+    fn create_patient_win(patient_name: &String, p_dir: &PathBuf) {
+
+        let save_btn = Button::new_with_label("Save");
+
+        let header_box = Box::new(Orientation::Vertical, 10);
+        let entry_text = Entry::new();
+
+        let hbox = Box::new(Orientation::Horizontal, 10);
+        hbox.pack_start(&entry_text, true, true, 10);
+        hbox.pack_start(&header_box, false, false, 10);
+
+        let (diag, de, ds, nd) = Self::read_patient_header(&p_dir);
+        for (idx, header) in [diag, de, ds, nd].iter().enumerate() {
+            match idx {
+                0 => {
+                    let label = Label::new("Diag: ");
+                    let entry = Entry::new();
+                    entry.set_text(&header);
+
+                    let hbox = Box::new(Orientation::Horizontal, 5);
+                    hbox.add(&label);
+                    hbox.add(&entry);
+
+                    header_box.add(&hbox);
+                },
+                1 => {
+                    let label = Label::new("DE:  ");
+                    let entry = Entry::new();
+                    entry.set_text(&header);
+
+                    let hbox = Box::new(Orientation::Horizontal, 5);
+                    hbox.add(&label);
+                    hbox.add(&entry);
+
+                    header_box.add(&hbox);
+                },
+                2 => {
+                    let label = Label::new("DS:  ");
+                    let entry = Entry::new();
+                    entry.set_text(&header);
+
+                    let hbox = Box::new(Orientation::Horizontal, 5);
+                    hbox.add(&label);
+                    hbox.add(&entry);
+
+                    header_box.add(&hbox);
+                },
+                3 => {
+                    let label = Label::new("ND:  ");
+                    let entry = Entry::new();
+                    entry.set_text(&header);
+
+                    let hbox = Box::new(Orientation::Horizontal, 5);
+                    hbox.add(&label);
+                    hbox.add(&entry);
+
+                    header_box.add(&hbox);
+                },
+                _ => panic!("??")
+            }
+
+        }
+
+        let vbox = Box::new(Orientation::Vertical, 10);
+
+        vbox.pack_start(&hbox, true, true, 10);
+        vbox.pack_start(&save_btn, false, false, 10);
+
+        let entry_win = Window::new(WindowType::Toplevel);
+        entry_win.add(&vbox);
+        entry_win.set_title(&patient_name);
+        entry_text.set_text(&Self::read_patient_data(&p_dir).1);
+
+        // clone ref
+        let patient_name = RefCell::new(patient_name.clone());
+        let ew = entry_win.clone();
+        let et = entry_text.clone();
+
+        let p_dir = p_dir.clone();
+
+        save_btn.connect_clicked(move |_| {
+            let p_name = patient_name.borrow().to_string();
+            let p_data = et.get_text().unwrap().to_string();
+            Self::sig_save_patient_data(&p_dir, p_name, p_data);
+            ew.destroy();
+        });
+
+        entry_win.show_all();
     }
 
     // manipulate patient data
@@ -316,6 +396,48 @@ impl Med {
         let patient_data: String = data.into_iter().skip(1).collect();
         (Some(patient_name), patient_data)
     }
+
+    fn read_patient_header(p_dir: &PathBuf) -> (String, String, String, String) {
+        let header_file = {
+            let mut d = p_dir.clone();
+            d.push("header");
+            d
+        };
+
+        if !std::path::Path::exists(&header_file) {
+            return Default::default();
+        }
+
+        let data = {
+            let mut data = String::new();
+            let mut d = fs::File::open(header_file).unwrap();
+            d.read_to_string(&mut data).unwrap();
+            data.to_string()
+        };
+
+        let cols: Vec<String> = data
+            .lines()
+            .enumerate()
+            .map(|(i, l)| match i {
+                0 => l.split("Diag: ").nth(1).unwrap().to_string(),
+                1 => l.split("DE: ").nth(1).unwrap().to_string(),
+                2 => l.split("DS: ").nth(1).unwrap().to_string(),
+                3 => l.split("ND: ").nth(1).unwrap().to_string(),
+                _ => {
+                    eprintln!("Patient header parsing Error!");
+                    Default::default()
+                }
+            })
+            .collect();
+
+        (
+            cols[0].clone(),
+            cols[1].clone(),
+            cols[2].clone(),
+            cols[3].clone(),
+        )
+    }
+
     // helpers
     fn med_dir() -> std::path::PathBuf {
         let mut med_dir = dirs::config_dir().unwrap();
